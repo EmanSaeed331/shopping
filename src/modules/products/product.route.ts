f7db6dc23bb01b812fe5ec8f7ff0b489d23c6697
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-08-01 00:38:49 +0200

    update modeule

--- src/modules/products/product.route.ts
+++ src/modules/products/product.route.ts
@@ -1,12 +1,12 @@
 import { Router, Request, Response } from 'express';
-import { productController } from './products.controller';
+import { productsController } from './products.controller';
 
 const productRouter = Router();
 
 
-productRouter.post('/',(req:Request,res:Response)=>{productController.createProduct(req,res)});
-productRouter.patch('/:id',(req:Request,res:Response)=> {productController.updateProduct(req,res)});
-productRouter.delete('/:id',(req:Request,res:Response)=> {productController.deleteProduct(req,res)})
-productRouter.post('/login',(req:Request , res:Response) =>{productController.getProductById(req,res)})
+productRouter.post('/',(req:Request,res:Response)=>{productsController.createProduct(req,res)});
+productRouter.patch('/:id',(req:Request,res:Response)=> {productsController.updateProduct(req,res)});
+productRouter.delete('/:id',(req:Request,res:Response)=> {productsController.deleteProduct(req,res)})
+productRouter.post('/login',(req:Request , res:Response) =>{productsController.getProductById(req,res)})
 
 export default productRouter;
--- src/modules/stores/store.route.ts
+++ src/modules/stores/store.route.ts
@@ -7,6 +7,6 @@ const storeRouter = Router();
 storeRouter.post('/',(req:Request,res:Response)=>{storeController.createStore(req,res)});
 storeRouter.patch('/:id',(req:Request,res:Response)=> {storeController.updateStore(req,res)});
 storeRouter.delete('/:id',(req:Request,res:Response)=> {storeController.deleteStore(req,res)})
-storeRouter.post('/login',(req:Request , res:Response) =>{storeController.getStoreById(req,res)})
+storeRouter.post('/login',(req:Request , res:Response) =>{storeController.getStore(req,res)})
 
 export default storeRouter;
